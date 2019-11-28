aa0c439c4687135b37aabd9435d6536c8b7ab1c1
Graeme Stewart <gi_stewart@yahoo.com>
2019-11-27 23:26:55 -0500

    adds initial code for order confirmation step

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
     var productList = $("tbody");
     var productContainer = $(".product-container");
+    let quantityRequested = $("#quantityRequested");
+
+    //Event listeners
     $(document).on("click", ".order-product", handleAddToCartPress);
+    // $(document).on("click", ".check-availability", handleCheckAvailability);
 
     getProducts();
 
@@ -13,7 +17,8 @@ $(document).ready(function() {
         newTr.data("product", data);
         newTr.append("<td>" + data.product_name + "</td>");
         newTr.append("<td>" + data.price + "</td>");
-        newTr.append("<td> <button class='order-product btn btn-warning'>Add to cart</button> </td>");
+        newTr.append("<td> <button class='order-product btn btn-warning' >Add to cart</button></td>");
+        // newTr.append("<td> <a href='/cart.html' class='order-product btn btn-warning' role = 'button'>Add to cart</a></td>");
         return newTr;
     }
 
@@ -28,7 +33,7 @@ $(document).ready(function() {
         });
     }
 
-    // A function for rendering the list of products to the page
+    // function for rendering the list of products to the page
     function renderProductList(rows) {
         productList.children().not(":last").remove();
         productContainer.children(".alert").remove();
@@ -40,11 +45,35 @@ $(document).ready(function() {
         }
     }
 
+    let listItemData = "";
+    let stockQuantity;
+
     // Function for handling what happens when the Add to cart button is pressed
     function handleAddToCartPress() {
         var listItemData = $(this).parent("td").parent("tr").data("product");
         var id = listItemData.id;
         console.log(id);
+
+        productName = listItemData.product_name;
+        console.log(productName);
+        $("#productSelected").text(productName);
+
+        productPrice = listItemData.price;
+        console.log(productPrice);
+        $("#productPrice").text(productPrice);
+
+        stockQuantity = listItemData.stock_quantity;
+        console.log(stockQuantity);
+
+        handleCheckAvailability2();
+    }
+
+    // Function to display cart details
+    // function displayCart() {}
+
+    function handleCheckAvailability2() {
+        console.log("test message");
+        console.log(stockQuantity);
     }
 
 });
